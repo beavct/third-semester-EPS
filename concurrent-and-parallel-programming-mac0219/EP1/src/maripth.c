750fbc2ff7a1dafa3b5eb649222aa499f3a68423
marigit2239 <139934639+marigit2239@users.noreply.github.com>
2023-07-20 03:03:56 -0300

    Update maripth.c

--- concurrent-and-parallel-programming-mac0219/EP1/src/maripth.c
+++ concurrent-and-parallel-programming-mac0219/EP1/src/maripth.c
@@ -10,6 +10,8 @@ double max = -1;
 struct task {
     byte *g_in;
     byte *g_out;
+    int g_min;
+    int g_max;
     int g_size;
     int n_threads;
 };
@@ -42,50 +44,44 @@ static byte get_next_cell(int i, int j, byte *grid_in, int grid_size) {
 void * teste(void *arg){
     struct task *t = (struct task *)arg;
     int grid_size=t->g_size;
-    for (int i = 0; i < t->g_size; i++) {
+    for (int i = t->g_min; i < t->g_max; i++) {
         for (int j = 0; j < t->g_size; j++) {
-            pthread_mutex_lock(&lock);
                 if (t->g_in[ind2d(i,j)] == WALL)
                     t->g_out[ind2d(i,j)] = WALL;
                 else
                     t->g_out[ind2d(i,j)] = get_next_cell(i, j, t->g_in, t->g_size);
-            pthread_mutex_unlock(&lock);
         }
     }
     return NULL;
 }
 
-static void update(byte *grid_in, byte *grid_out, int grid_size, int num_threads) {
+static void update(byte *grid_in, byte *grid_out, int grid_size, int num_threads,struct task* tasks) {
     pthread_t *threads;
-    pthread_mutex_init(&lock, NULL);
     threads = malloc(num_threads * sizeof(pthread_t));
-    struct task *tasks;
-    int threads_with_one_more_work = grid_size % num_threads;
-    
-    //criar variavel
-    tasks = malloc(num_threads * sizeof(struct task));
-
-    for (int i = 0; i < num_threads; ++i) {
-        int work_size = grid_size / num_threads;
-        if (i < threads_with_one_more_work)
-            work_size += 1;
+    for (int i = 0; i < num_threads; i++) {
         tasks[i].g_in = grid_in;
         tasks[i].g_out = grid_out;
-        tasks[i].g_size = grid_size;
-        tasks[i].n_threads = num_threads;
         pthread_create(&threads[i], NULL, teste, (void*)&tasks[i]);
     }
 
-    // Finaliza threads, ignorando o valor de retorno
-    for (int i = 0; i < num_threads; ++i) {
+    for (int i = 0; i < num_threads; i++) {
         pthread_join(threads[i], NULL);
     }
     
 }
 
 void simulate_pth(byte *grid_1, byte *grid_2, int grid_size, int num_threads) {
+    struct task *tasks;
+    tasks = malloc(num_threads * sizeof(struct task));
+    int param=grid_size/num_threads;
+    for (int i = 0; i < num_threads; i++) {
+        tasks[i].g_min = i*param;
+        tasks[i].g_max = (i+1)*param;
+        tasks[i].g_size = grid_size;
+        tasks[i].n_threads = num_threads;
+    }
     for (int i = 0; i < ITERATIONS/2; i++) {
-        update(grid_1, grid_2, grid_size,num_threads);
-        update(grid_2, grid_1, grid_size,num_threads);
+        update(grid_1, grid_2, grid_size,num_threads,tasks);
+        update(grid_2, grid_1, grid_size,num_threads,tasks);
     }
 }
